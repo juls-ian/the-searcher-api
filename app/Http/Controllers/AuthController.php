c78303ef49dff4ef76b8cb818864c12171bc4221
Julius Ian Valdez <julius2614@gmail.com>
2025-06-10 22:57:42 +0800

    feat(authentication): :sparkles: add login & logout user
    created the login and logout user feature, with token creation

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        $user = $request->authenticate();
+
+        // Create API token 
+        $token = $user->createToken('auth-token')->plainTextToken;
+        return response()->json([
+            'success' => true,
+            'message' => 'Login successful',
+            'data' => [
+                'user' => $user,
+                'token' => $token,
+                'token_type' => 'Bearer'
+            ]
+        ]);
+
+    }
+
+    public function logout(Request $request)
+    {
+
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Logout successfully'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,15 +10,26 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/', function () {
-    return 'HI';
+
+
+// User Auth routes
+Route::prefix('auth')->group(function () {
+
+    Route::post('/login', [AuthController::class, 'login']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+
+        Route::post('/logout', [AuthController::class, 'logout']);
+    });
+
 });
 
+// Article routes 
 Route::controller(ArticleController::class)->group(function () {
     Route::apiResource('articles', ArticleController::class);
 })->middleware('guest');
 
-
+// User routes
 Route::controller(UserController::class)->group(function () {
     Route::apiResource('users', UserController::class);
 });
