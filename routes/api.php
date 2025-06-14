00861d66d9cfb99951ac78be1b489b008b9c3b70
Julius Ian Valdez <julius2614@gmail.com>
2025-06-14 22:12:06 +0800

    chore(controller): :art: restructure auth controller
    changed the location of auth controller for future proofing and better organization

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Http\Controllers\ArticleController;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\UserController;
-use App\Http\Middleware\HandleExpiredTokens;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Middleware\HandleExpiredTokens;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
