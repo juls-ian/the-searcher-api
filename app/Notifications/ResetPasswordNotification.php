f7c2baf15bd1ec14ecb997fd000fd2369af459c8
juls-ian <juls.ian.dev@gmail.com>
2025-06-15 21:12:16 +0800

    feat(authentication): :sparkles: add forgot and reset password
    new feature where users will be able to recover their account

---
+++ app/Notifications/ResetPasswordNotification.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ResetPasswordNotification extends Notification
+{
+    use Queueable;
+
+    protected $token;
+    protected $resetUrl;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($token, $resetUrl = null)
+    {
+        $this->token = $token;
+        #     ? if not null or not falsy : fallback if null or falsy
+        $this->resetUrl = $resetUrl ?: config('app.frontend_url') . '/reset-password';
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        $url = $this->resetUrl . '?' . http_build_query([
+            'token' => $this->token,
+            'email' => $notifiable->getEmailForPasswordReset()
+        ]);
+        return (new MailMessage)
+            ->subject('Reset Password')
+            ->line('The introduction to the notification.')
+            ->action('Notification Action', $url)
+            ->line('This password reset link will expire in ' . config('auth.passwords.users.expire') . ' minutes.')
+            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
