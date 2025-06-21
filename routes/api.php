318f08c1ce47a63e105c1ff77b457199bfff974a
juls-ian <juls.ian.dev@gmail.com>
2025-06-21 22:35:14 +0800

    refactor(email-verification): :recycle: refactor the email verification route and update api & workflow docs
    refactored the email verification routes for better maintainability and updated the documentations of the api route and workflow

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\Auth\ResetPasswordController;
 use App\Http\Controllers\Auth\SetPasswordController;
+use App\Http\Controllers\EmailVerificationController;
 use App\Http\Middleware\HandleExpiredTokens;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
@@ -58,32 +59,19 @@ Route::apiResource('users', UserController::class)->middleware('guest'); # chang
  */
 Route::prefix('email')->middleware('auth:sanctum')->group(function () {
 
-
     // Email verification notice 
-    Route::post('/verify', function (Request $request) {
-        if ($request->user()->hasVerifiedEmail()) {
-            return response()->json(['message' => 'User is already verified'], 400);
-        }
-
-        $request->user()->sendEmailVerificationNotification();
-        return response()->json(['message' => 'A verification email was sent.']);
-
-    })->middleware(['throttle:5,1'])
+    Route::post('/verify', [EmailVerificationController::class, 'send'])
+        ->middleware(['throttle:5,1'])
         ->name('verification.notice');
 
     // Email verification handler
-    Route::get('/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-        $request->fulfill();
-
-        return response()->json(['message' => 'Email successfully verified']);
-    })->middleware(['signed', 'throttle:6,1'])
+    Route::get('/verify/{id}/{hash}', [EmailVerificationController::class, 'verify'])
+        ->middleware(['signed', 'throttle:6,1'])
         ->name('verification.verify');
 
     // Resending verification email 
-    Route::post('/verification-notification', function (Request $request) {
-        $request->user()->sendEmailVerificationNotification();
-
-        return response()->json(['message' => 'Verification link resent']);
-    })->middleware('throttle:6,1')->name('verification.send');
+    Route::post('/verification-notification', [EmailVerificationController::class, 'resend'])
+        ->middleware('throttle:6,1')
+        ->name('verification.send');
 
 });
